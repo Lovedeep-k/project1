aeb4984fa176eb7695637b0c9078ff89cd939265
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2025-11-21 13:14:34 +0530

    initial commit

--- vaibhavcafe/settings.py
+++ vaibhavcafe/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rmp1qgag-d_z05_d8nk1+ol3p-plzc=ky#95oz)1fxw59b$jau
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vaibhavcafe.onrender.com','localhost''*','127.0.0.1']
 
 
 # Application definition
@@ -115,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -123,8 +125,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
+    # BASE_DIR / "static",
+    # "/var/www/static/",
 ]
 import os
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'images')
